c33a446cc9fbd337ce4050fda31072e1e3d3ed97
jessikaforster <jessikaforster@icloud.com>
2021-09-13 13:38:23 -0400

    Drawing experiment: added comments

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,54 +1,36 @@
 /**
-Title of Project
-Author Name
+Drawing Experiments
+Jessika Forster
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Experimenting with p5's drawing and color functions.
 */
 
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
+// setup()
+//
+// Draws a face on the canvas
 function setup() {
   createCanvas(500,500);
 
+  // Set the background to sky blue
   background(50, 100, 250);
 
-ellipseMode(CORNER);
+  // Draw a flesh-coloured head
+  fill(250,200,200);
+  ellipse(250,250,200,200);
 
-stroke(255,255,255)
+  // Draw the eyes black
+  fill(0,0,0);
+  ellipse(200,250,30,30);
+  ellipse(300,250,30,30);
 
-fill(127,0,200, 100);
-ellipse(250,250,100,100);
-
-fill(137,0,210, 100);
-ellipse(250,250,80,80);
-
-fill(147,0,220, 100);
-ellipse(250,250,60,60);
-
-fill(157,0,230, 100);
-ellipse(250,250,40,40);
-
-fill(167,0,240, 100);
-ellipse(250,250,20,20);
+  // Draw the mouth
+  strokeWeight(10);
+  line(200,300,300,300);
 }
 
-
-/**
-Description of draw()
-*/
+// Draw()
+//
+// Does nothing
 function draw() {
 
 }
