0e422c2203bb173f1afb832465d15630c9d03c51
jessikaforster <jessikaforster@icloud.com>
2021-09-23 18:47:54 -0400

    Added more squares and added more required features

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -28,9 +28,39 @@ let rect1 = {
     stroke: 255,
     alpha: 255,
     speed: 1,
-  }
+  };
 
-let rect3 = {
+  let rect3 = {
+    x: 250,
+    y: 250,
+    size: 500,
+    fill: 255,
+    stroke: 255,
+    alpha: 255,
+    speed: 1,
+  };
+
+  let rect4 = {
+    x: 250,
+    y: 250,
+    size: 500,
+    fill: 255,
+    stroke: 255,
+    alpha: 255,
+    speed: 1,
+  };
+
+  let rect5 = {
+    x: 250,
+    y: 0,
+    size: 100,
+    fill: 255,
+    stroke: 255,
+    alpha: 255,
+    speed: 1,
+  };
+
+let rect6 = {
   x: 250,
   y: 250,
   size: 100,
@@ -38,7 +68,7 @@ let rect3 = {
   stroke: 255,
   alpha: 255,
   speed: 1,
-}
+};
 
 /**
 Description of setup
@@ -78,12 +108,40 @@ rectMode(CENTER);
 rect2.x = rect2.x - 1;
 rect(rect2.x,rect2.y,rect2.size);
 rect2.size = rect2.size + 0.75;
-rect2.size = constrain(rect2.size,0,300);
+// rect2.size = constrain(rect2.size,0,300);
 
-
-// Rect 3 movement
-fill(rect3.fill);
+// Rect3 movement
+noFill();
+stroke(rect3.stroke,rect3.alpha);
+strokeWeight(5);
 rectMode(CENTER);
-rect3.size = random(-10,200);
+rect3.x = rect3.x + 1;
 rect(rect3.x,rect3.y,rect3.size);
+rect3.size = rect3.size - 0.50;
+
+// Rect4 movement
+noFill();
+stroke(rect4.stroke,rect4.alpha);
+strokeWeight(5);
+rectMode(CENTER);
+rect4.x = rect4.x - 1;
+rect(rect4.x,rect4.y,rect4.size);
+rect4.size = rect4.size - 0.50;
+
+// Rect5 movement
+noFill();
+stroke(rect5.stroke,rect5.alpha);
+strokeWeight(5);
+// rectMode(CENTER);
+rect5.size = constrain(rect5.size,100,300);
+rect(rect5.x,rect5.y,rect5.size);
+rect5.size = rect5.size + 1;
+rect5.y = rect5.y + 1;
+
+
+// Rect 6 movement
+fill(rect6.fill - 10);
+rectMode(CENTER);
+rect6.size = random(0,300);
+rect(rect6.x,rect6.y,rect6.size);
 }
