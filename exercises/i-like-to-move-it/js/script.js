58fd56d7a26e002c266165a35eb3ed929e8b3306
jessikaforster <jessikaforster@icloud.com>
2021-09-23 19:33:47 -0400

    Revert "Added detailed descriptions of steps"
    This reverts commit 8bccc68796a1d0955f8644f5ef79fa7d78907fea.

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -69,7 +69,7 @@ let rect1 = {
 };
 
 /**
-Create a canvas that has no stroke
+Description of setup
 */
 function setup() {
     createCanvas(500,500);
@@ -78,18 +78,18 @@ function setup() {
 
 
 /**
-Colour changing background, squares that only have stroke and no fill that are shrinking, growing, moving left
-and right and moving down and a square at the center that is filled with no stroke that is growing and shrinking randomly
+Description of draw()
 */
 
+  // First growing square
   function draw() {
 
-// Background that changes colour with mouse movements on the x and y axes
+// Background
     background(bg.r,bg.g,bg.b);
     bg.r = map(mouseX, 0, width, 255, 0);
     bg.b = map(mouseY,0,height, 0, 255);
 
-// Rect1 growing and moving to the right
+// Rect1 movement
     noFill();
     stroke(rect1.stroke,rect1.alpha);
     strokeWeight(5);
@@ -98,7 +98,7 @@ and right and moving down and a square at the center that is filled with no stro
     rect(rect1.x,rect1.y,rect1.size);
     rect1.size = rect1.size + 0.75;
 
-// Rect2 growing and moving to the left
+// Rect2 movement
     noFill();
     stroke(rect2.stroke,rect2.alpha);
     strokeWeight(5);
@@ -107,7 +107,7 @@ and right and moving down and a square at the center that is filled with no stro
     rect(rect2.x,rect2.y,rect2.size);
     rect2.size = rect2.size + 0.75;
 
-// Rect3 shrinking and moving to the right
+// Rect3 movement
     noFill();
     stroke(rect3.stroke,rect3.alpha);
     strokeWeight(5);
@@ -116,7 +116,7 @@ and right and moving down and a square at the center that is filled with no stro
     rect(rect3.x,rect3.y,rect3.size);
     rect3.size = rect3.size - 0.50;
 
-// Rect4 shrinking and moving to the left
+// Rect4 movement
     noFill();
     stroke(rect4.stroke,rect4.alpha);
     strokeWeight(5);
@@ -125,7 +125,7 @@ and right and moving down and a square at the center that is filled with no stro
     rect(rect4.x,rect4.y,rect4.size);
     rect4.size = rect4.size - 0.50;
 
-// Rect5 growing and shrinking while moving from top to bottom
+// Rect5 movement
     noFill();
     stroke(rect5.stroke,rect5.alpha);
     strokeWeight(5);
@@ -135,7 +135,7 @@ and right and moving down and a square at the center that is filled with no stro
     rect5.y = rect5.y + 1;
 
 
-// Rect 6 random movement
+// Rect 6 movement
     noStroke();
     fill(rect6.fill);
     rectMode(CENTER);
