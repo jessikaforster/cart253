f0e7af276c459a52c1e66837434a39f03134cef5
jessikaforster <jessikaforster@icloud.com>
2021-09-23 19:11:57 -0400

    Minor adjustments to shapes and movements

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -60,22 +60,20 @@ let rect1 = {
     speed: 1,
   };
 
-let rect6 = {
-  x: 250,
-  y: 250,
-  size: 100,
-  fill: 255,
-  stroke: 255,
-  alpha: 255,
-  speed: 1,
+  let rect6 = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: 255,
+    alpha: 255,
 };
 
 /**
 Description of setup
 */
 function setup() {
-createCanvas(500,500);
-noStroke();
+    createCanvas(500,500);
+    noStroke();
 }
 
 
@@ -92,56 +90,55 @@ Description of draw()
     bg.b = map(mouseY,0,height, 0, 255);
 
 // Rect1 movement
-noFill();
-stroke(rect1.stroke,rect1.alpha);
-strokeWeight(5);
-rectMode(CENTER);
-rect1.x = rect1.x + 1;
-rect(rect1.x,rect1.y,rect1.size);
-rect1.size = rect1.size + 0.75;
+    noFill();
+    stroke(rect1.stroke,rect1.alpha);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect1.x = rect1.x + 1;
+    rect(rect1.x,rect1.y,rect1.size);
+    rect1.size = rect1.size + 0.75;
 
 // Rect2 movement
-noFill();
-stroke(rect2.stroke,rect2.alpha);
-strokeWeight(5);
-rectMode(CENTER);
-rect2.x = rect2.x - 1;
-rect(rect2.x,rect2.y,rect2.size);
-rect2.size = rect2.size + 0.75;
-// rect2.size = constrain(rect2.size,0,300);
+    noFill();
+    stroke(rect2.stroke,rect2.alpha);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect2.x = rect2.x - 1;
+    rect(rect2.x,rect2.y,rect2.size);
+    rect2.size = rect2.size + 0.75;
 
 // Rect3 movement
-noFill();
-stroke(rect3.stroke,rect3.alpha);
-strokeWeight(5);
-rectMode(CENTER);
-rect3.x = rect3.x + 1;
-rect(rect3.x,rect3.y,rect3.size);
-rect3.size = rect3.size - 0.50;
+    noFill();
+    stroke(rect3.stroke,rect3.alpha);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect3.x = rect3.x + 1;
+    rect(rect3.x,rect3.y,rect3.size);
+    rect3.size = rect3.size - 0.50;
 
 // Rect4 movement
-noFill();
-stroke(rect4.stroke,rect4.alpha);
-strokeWeight(5);
-rectMode(CENTER);
-rect4.x = rect4.x - 1;
-rect(rect4.x,rect4.y,rect4.size);
-rect4.size = rect4.size - 0.50;
+    noFill();
+    stroke(rect4.stroke,rect4.alpha);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect4.x = rect4.x - 1;
+    rect(rect4.x,rect4.y,rect4.size);
+    rect4.size = rect4.size - 0.50;
 
 // Rect5 movement
-noFill();
-stroke(rect5.stroke,rect5.alpha);
-strokeWeight(5);
-// rectMode(CENTER);
-rect5.size = constrain(rect5.size,100,300);
-rect(rect5.x,rect5.y,rect5.size);
-rect5.size = rect5.size + 1;
-rect5.y = rect5.y + 1;
+    noFill();
+    stroke(rect5.stroke,rect5.alpha);
+    strokeWeight(5);
+    rect5.size = constrain(rect5.size,100,300);
+    rect(rect5.x,rect5.y,rect5.size);
+    rect5.size = rect5.size + 1;
+    rect5.y = rect5.y + 1;
 
 
 // Rect 6 movement
-fill(rect6.fill - 10);
-rectMode(CENTER);
-rect6.size = random(0,300);
-rect(rect6.x,rect6.y,rect6.size);
+    noStroke();
+    fill(rect6.fill);
+    rectMode(CENTER);
+    rect6.size = random(0,300);
+    rect(rect6.x,rect6.y,rect6.size);
 }
