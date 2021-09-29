a482368623156b148288f384455961c71e150b64
jessikaforster <jessikaforster@icloud.com>
2021-09-29 19:39:54 -0400

    E2: Imported all photos to be used

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,18 +1,19 @@
 /**
-Title of Project
-Author Name
+Dodge-Em
+Jessika Forster
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Exercise to practice writing if-statements, working with loops for drawing, playing with the mouse
+and learning to display image(s).
 */
-
-"use strict";
-
 
 /**
 Description of preload
 */
 function preload() {
+  gunImage = loadImage("assets/images/gun.png");
+  alien1Image = loadImage("assets/images/alien1.png");
+  alien2Image = loadImage("assets/images/alien2.png");
+  alien3Image = loadImage("assets/images/alien3.png");
 
 }
 
@@ -21,7 +22,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
 }
 
 
@@ -29,5 +30,11 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  image(alien1Image,0,0,100,100);
+  image(gunImage,0,0,100,100);
+  //image(alien1Image,50,50,100,100);
+  image(alien2Image,0,0,100,100);
+  image(alien3Image,0,0,100,100);
 }
