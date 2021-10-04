8ddcb819735ba11a7f0f29ce0615d2f815fece35
jessikaforster <jessikaforster@icloud.com>
2021-10-03 23:35:53 -0400

    E3: Started Love, Actually exercise

---
+++ exercises/love-actually/js/script.js
@@ -0,0 +1,22 @@
+/**
+Love Actually
+Jessika Forster
+
+This is the 3rd exercise for CART 253. The program must allow for the user to control one of the circles, the non-user circle should move differently,
+at least one extra function must be added and at least one extra ending is required.
+*/
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
