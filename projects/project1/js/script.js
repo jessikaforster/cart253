b617d291f0af8f3d16b01fd5305fbaf2ce0a8ec9
jessikaforster <jessikaforster@icloud.com>
2021-10-10 15:54:22 -0400

    P1: Testing out gravity

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Project 1
+Jessika Forster
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,6 +8,21 @@ author, and this description to match your project!
 
 "use strict";
 
+let circle = {
+  x: 0,
+  y: 0,
+  size: 100,
+  r: 155,
+  g: 70,
+  b: 55,
+  speed: -3,
+  speedX: 0,
+  speedY: 0,
+  vx: 0,
+  vy: -2,
+  gravity: 1,
+  gravitySpeed: 0,
+}
 
 /**
 Description of preload
@@ -21,7 +36,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
 }
 
 
@@ -29,5 +44,25 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(0);
+displayCircle();
+circleGravity();
+circleMovement();
+}
 
+function circleGravity() {
+  circle.gravitySpeed += circle.gravity;
+  circle.x += circle.speedX;
+  circle.y += circle.speedY + circle.gravitySpeed;
+}
+
+function circleMovement() {
+  circle.speed += circle.vy;
+  circle.y += circle.speed - circle.vy
+}
+
+function displayCircle() {
+ellipse(circle.x,circle.y,circle.size,circle.r,circle.g,circle.b);
+circle.x = windowWidth/2;
+circle.y = windowHeight/2;
 }
