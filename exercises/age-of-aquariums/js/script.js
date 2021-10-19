9dc43a418003e2d22a5e1fcac78090221628e6ad
jessikaforster <jessikaforster@icloud.com>
2021-10-19 11:54:12 -0400

    E4: Display candy1 and user + user can eat candies

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -1,178 +1,93 @@
-/**
-Age of Aquariums
-Jessika Forster
+"use strict";
 
-Exercise to practice adding a user-controlled shape (or image), making the user interact with the fish
-(or whatever they are in your simulation), changing the fish (or whatever) creation and adding at least 2
-endings.
-*/
-let candies1 = []
-let candy1Size = 10;
-let candies2 = []
-let candy2Size = 10;
-let candies3 = []
-let candy3Size = 10;
-let enemies = []
-let enemySize = 10;
-let bg;
-
-function preload() {
-  bg = loadImage("assets/images/street.gif");
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-let state = `simulation`; // Can be: title, simulation, end1, end2
-
-// User that will be moved using the mouse
+// Our user, to move with the mouse
 let user = {
   x: 0,
   y: 0,
-  size: 100,
+  size: 100
 };
 
-function createCandy1(x,y) {
-let candy1 = {
-    x: x,
-    y: y,
-    size: 100,
-    vx: 0,
-    vy: 0,
-    speed: 2,
-    eaten: false,
-  };
-  return candy1;
+// Foods
+let candy1;
+let candy2;
+let candy3;
+let candy4;
+let candy5;
+let candy6;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  candy1 = createCandy(250,windowHeight/2);
+  candy2 = createCandy(350,windowHeight/2);
+  candy3 = createCandy(450,windowHeight/2);
+  candy4 = createCandy(550,windowHeight/2);
+  candy5 = createCandy(650,windowHeight/2);
+  candy6 = createCandy(750,windowHeight/2);
 }
 
-function createCandy2(x,y) {
-let candy2 = {
+function createCandy(x,y) {
+  let candy = {
     x: x,
     y: y,
-    size: 100,
-    vx: 0,
-    vy: 0,
-    speed: 2,
-    eaten: false,
+    size: 50,
+    eaten: false
   };
-  return candy2;
-}
-
-function createCandy3(x,y) {
-let candy3 = {
-    x: x,
-    y: y,
-    size: 100,
-    vx: 0,
-    vy: 0,
-    speed: 2,
-    eaten: false,
-  };
-  return candy3;
-}
-
-function createEnemy(x,y) {
-let enemy = {
-    x: x,
-    y: y,
-    size: 100,
-    vx: 0,
-    vy: 0,
-    speed: 2
-  };
-  return enemy;
+  return candy;
 }
 
 function draw() {
-  background(bg);
+  background(0);
 
-// Identifying all states
-if (state === `title`) {
-    title();
-  }
-  else if (state === `simulation`) {
-    simulation();
-  }
-  else if (state === `end1`) {
-    end1();
-  }
-  else if (state === `end2`) {
-    end2();
-    }
-  }
+  // Move the user (with the mouse)
+  moveUser();
 
-  function title() {
+  // Check whether the user has eaten either food
+  checkCandy(candy1);
+  checkCandy(candy2);
+  checkCandy(candy3);
+  checkCandy(candy4);
+  checkCandy(candy5);
+  checkCandy(candy6);
 
-  }
+  // Display the user and foods
+  displayUser();
+  displayCandy(candy1);
+  displayCandy(candy2);
+  displayCandy(candy3);
+  displayCandy(candy4);
+  displayCandy(candy5);
+  displayCandy(candy6);
+}
 
-  function simulation() {
-    moveUser();
-    checkCandy1();
-    displayUser();
-    makeCandy1();
-  }
-
-  function end1() {
-
-  }
-
-  function end2() {
-
-  }
-
-  // Sets the user position to the mouse position
+// Sets the user position to the mouse position
 function moveUser() {
   user.x = mouseX;
   user.y = mouseY;
 }
 
-function checkCandy1(candy1) {
-  if (!candy1.eaten) {
-    let d = dist(user.x, user.y, candy1.x, candy1.y);
-    if (d < user.size / 2 + candy1.size / 2) {
-      candy1.eaten = true;
+function checkCandy(candy) {
+  if (!candy.eaten) {
+    let d = dist(user.x, user.y, candy.x, candy.y);
+    if (d < user.size / 2 + candy.size / 2) {
+      candy.eaten = true;
     }
   }
-
-  // Display all circles
-  function displayUser() {
-    push();
-    fill(255,102,0);
-    ellipse(user.x, user.y, user.size);
-    pop();
-  }
-  // Show apple and add movement
-  function makeCandy1() {
-      for (let i = 0; i < candies1.length; i++) {
-        moveCandy1(candies1[i]);
-        displayCandy1(candies1[i]);
-    }
 }
 
-function moveCandy1(candy1) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    candy1.vx = random(-candy1.speed, candy1.speed);
-    candy1.vy = random(-candy1.speed, candy1.speed);
-  }
-      // Move the candy1
-  candy1.x = candy1.x + candy1.vx;
-  candy1.y = candy1.y + candy1.vy;
+// Draw the user as a circle
+function displayUser() {
+  push();
+  fill(255);
+  ellipse(user.x, user.y, user.size);
+  pop();
+}
 
-  // Constrain the fish to the canvas
-  candy1.x = constrain(candy1.x, 0, width);
-  candy1.y = constrain(candy1.y, 0, height);
-    }
-  }
-
-
-  function displayCandy1(candy1) {
-    if (!candy1.eaten){
+function displayCandy(candy) {
+  if (!candy.eaten){
     push();
-    fill(0,187,255);
-    ellipse(candy1.x,candy1.y,candy1.size);
+    fill(255,100,100);
+    ellipse(candy.x,candy.y,candy.size);
     pop();
-    }
   }
+}
