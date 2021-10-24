2562de764f29a10da1069d4a97cefad4a3c4ea39
jessikaforster <jessikaforster@icloud.com>
2021-10-24 13:28:50 -0400

    8: Finished videos 8.1 to 8.4

--- experiments/8.1 O.O. Programming/js/script.js
+++ experiments/8.1 O.O. Programming/js/script.js
@@ -8,26 +8,101 @@ author, and this description to match your project!
 
 "use strict";
 
+// Our garden
+let garden = {
+  // An array to store the individual flowers
+  flowers: [],
+  // How many flowers in the garden
+  numFlowers: 30,
+  // An array of bees
+  bees: [],
+  // How many bees in the garden
+  numBees: 7,
+  // The color of the grass (background)
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
+};
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
+// setup() creates the canvas and the flowers in the garden
 function setup() {
+  createCanvas(600, 600);
 
+  // Create our flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    let size = random(50,80);
+    let stemLength = random(50,100);
+    let petalColor = {
+      r: random(100,255),
+      g: random(100,255),
+      b: random(100,255),
+    };
+        // Create a new flower
+    let flower = new Flower(x, y, size, stemLength, petalColor);
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+
+  // NEW! Sort the array using the sortByY() function
+  garden.flowers.sort(sortByY);
 }
 
+// Create the bees
+for (let i = 0; i < garden.numBees; i++) {
+  let bee = new Bee(random(0,width),random(0,height));
+  garden.bees.push(bee);
+  }
+}
 
-/**
-Description of draw()
-*/
+// sortByY() takes two flowers as parameters to compare
+// It should return a negative number if flower1 should come
+// BEFORE flower2 in the array, a positive number if flower1 should
+// come AFTER flower2 in the array, and 0 if there they have the
+// same priority
+function sortByY(flower1, flower2) {
+  // We achieve the above by subtracting flower2's y position
+  // from flower1's! How elegant!
+  return flower1.y - flower2.y;
+  }
+
+// draw()
+// Displays our flowers
 function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
 
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    if (flower.alive) {
+    flower.shrink();
+    flower.display();
+    }
+  }
+
+  for (let i = 0; i < garden.bees.length; i++) {
+    let bee = garden.bees[i];
+    if (bee.alive) {
+      bee.shrink();
+      bee.move();
+      bee.display();
+
+      for (let j = 0; j < garden.flowers.length; j++) {
+        let flower = garden.flowers[j];
+        if (flower.alive) {
+        bee.tryToPollinate(flower);
+        }
+      }
+    }
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    flower.mousePressed();
+  }
 }
