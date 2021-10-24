d7642daae10becb7a80affedbabff8134312773a
jessikaforster <jessikaforster@icloud.com>
2021-10-24 14:37:43 -0400

    A-08: Completed object oriented programming activity

--- activities/08-object-oriented-programming-activity/js/script.js
+++ activities/08-object-oriented-programming-activity/js/script.js
@@ -1,27 +1,26 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
+let gravityForce = 0.0025;
 
-/**
-Description of preload
-*/
-function preload() {
+let paddle;
 
-}
-
+let balls = [];
+let numBalls = 10;
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
 }
 
 
@@ -29,5 +28,18 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+    ball.gravity(gravityForce);
+    ball.move();
+    ball.bounce(paddle);
+    ball.display();
+    }
+  }
 }
