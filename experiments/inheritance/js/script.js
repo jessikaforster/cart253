4b9c13ca23c54e37944768b90bd5736a0d30dab3
jessikaforster <jessikaforster@icloud.com>
2021-10-29 12:43:39 -0400

    9.4 Video completed

--- experiments/inheritance/js/script.js
+++ experiments/inheritance/js/script.js
@@ -11,6 +11,7 @@ author, and this description to match your project!
 let vehicles = [];
 let numCars = 10;
 let numMotorcycles = 10;
+let numSportsCars = 3;
 
 /**
 Description of preload
@@ -37,6 +38,13 @@ function setup() {
     let y = random(0,height);
     let motorcycle = new Motorcycle(x,y);
     vehicles.push(motorcycle);
+  }
+
+  for (let i = 0; i < numSportsCars; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    let sportscar = new SportsCar(x,y);
+    vehicles.push(sportscar);
   }
 }
 
