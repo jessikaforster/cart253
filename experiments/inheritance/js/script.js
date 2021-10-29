a745bd0e10dcd16b5fad679b243a1a8f964c9eaa
jessikaforster <jessikaforster@icloud.com>
2021-10-29 12:35:52 -0400

    9.3 Video completed

--- experiments/inheritance/js/script.js
+++ experiments/inheritance/js/script.js
@@ -8,10 +8,8 @@ author, and this description to match your project!
 
 "use strict";
 
-let cars = [];
+let vehicles = [];
 let numCars = 10;
-
-let motorcycles = [];
 let numMotorcycles = 10;
 
 /**
@@ -31,14 +29,14 @@ function setup() {
     let x = random(0,width);
     let y = random(0,height);
     let car = new Car(x,y);
-    cars.push(car);
+    vehicles.push(car);
   }
 
   for (let i = 0; i < numMotorcycles; i++) {
     let x = random(0,width);
     let y = random(0,height);
     let motorcycle = new Motorcycle(x,y);
-    motorcycles.push(motorcycle);
+    vehicles.push(motorcycle);
   }
 }
 
@@ -49,17 +47,10 @@ Description of draw()
 function draw() {
   background(0);
 
-  for (let i = 0; i < cars.length; i++) {
-    let car = cars[i];
-    car.move();
-    car.wrap();
-    car.display();
-  }
-
-  for (let i = 0; i < motorcycles.length; i++) {
-    let motorcycle = motorcycles[i];
-    motorcycle.move();
-    motorcycle.wrap();
-    motorcycle.display();
+  for (let i = 0; i < vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
   }
 }
