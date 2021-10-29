327c178ec6cab3707fdafa73260172206dfcbaa3
jessikaforster <jessikaforster@icloud.com>
2021-10-29 12:13:58 -0400

    9.1 Video completed

--- experiments/inheritance/js/script.js
+++ experiments/inheritance/js/script.js
@@ -8,6 +8,11 @@ author, and this description to match your project!
 
 "use strict";
 
+let cars = [];
+let numCars = 10;
+
+let motorcycles = [];
+let numMotorcycles = 10;
 
 /**
 Description of preload
@@ -21,7 +26,20 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(600,600);
+  for (let i = 0; i < numCars; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    let car = new Car(x,y);
+    cars.push(car);
+  }
 
+  for (let i = 0; i < numMotorcycles; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    let motorcycle = new Motorcycle(x,y);
+    motorcycles.push(motorcycle);
+  }
 }
 
 
@@ -29,5 +47,19 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  for (let i = 0; i < cars.length; i++) {
+    let car = cars[i];
+    car.move();
+    car.wrap();
+    car.display();
+  }
+
+  for (let i = 0; i < motorcycles.length; i++) {
+    let motorcycle = motorcycles[i];
+    motorcycle.move();
+    motorcycle.wrap();
+    motorcycle.display();
+  }
 }
