16ce705998d74ab560fb7a0d34f96148ddf48bfa
jessikaforster <jessikaforster@icloud.com>
2021-11-07 14:12:31 -0500

    EX: Identified all variables in Bluejay class

--- exercises/make-some-noise/js/Bluejay.js
+++ exercises/make-some-noise/js/Bluejay.js
@@ -1,3 +1,19 @@
-class Bluejay {
-  
+class Bluejay extends Bird {
+  constructor(x,y) {
+    super(x,y);
+    this.size = 40;
+    this.speed = 10;
+  }
+
+  display() {
+    super.display();
+
+  push();
+  ellipseMode(CENTER);
+  strokeWeight(4);
+  stroke(163,214,255);
+  fill(0,140,255)
+  ellipse(this.x,this.y,this.size);
+  pop();
+  }
 }
