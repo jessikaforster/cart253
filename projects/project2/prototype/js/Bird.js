83560e93fd8e0cf9cf175918bd8cbd2b6a1cd4f0
jessikaforster <jessikaforster@icloud.com>
2021-11-08 14:01:10 -0500

    P2.P: Created classes and identified variables in Bird class

---
+++ projects/project2/prototype/js/Bird.js
@@ -0,0 +1,32 @@
+class Bird {
+  constructor(x, y, image) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.ax = 0;
+    this.ay = 0;
+    this.width = 300;
+    this.height = 300;
+    this.speed = undefined;
+    this.image = image;
+  }
+
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  wrap() {
+    if (this.x > width) {
+      this.x -= width;
+    }
+    else if (this.x < 0) {
+      this.x += width
+    }
+  }
+
+  display() {
+
+  }
+}
--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -7,12 +7,29 @@ Prototype for project 2, final CART 253 project.
 
 "use strict";
 
+let birds = [];
+let numBluejays = 10;
+let numSparrows = 10;
+let numCardinals = 10;
+
 let bg;
+
+let userImage;
+
+let bluejayImage = undefined;
+let cardinalImage = undefined;
+let sparrowImage = undefined;
 
 /**
 Description of preload
 */
 function preload() {
+  bluejayImage = loadImage("assets/images/bluejay.gif");
+  cardinalImage = loadImage("assets/images/cardinal.gif");
+  sparrowImage = loadImage("assets/images/sparrow.gif");
+
+  userImage = loadImage("assets/images/reindeer.png");
+
   bg = loadImage("assets/images/bg.gif");
 }
 
