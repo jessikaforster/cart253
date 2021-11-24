51af434721e7f068a62437578713993c5b908e2a
jessikaforster <jessikaforster@icloud.com>
2021-11-23 19:45:02 -0500

    P2.F: Created snowflake class for level 5

--- projects/project2/progress/js/FireL5.js
+++ projects/project2/progress/js/FireL5.js
@@ -1,16 +1,16 @@
 class FireL5 {
   constructor(fireImage) {
     this.x = width / 2;
-    this.y = height;
-    this.width = 400;
-    this.height = 400;
+    this.y = height / 1.2;
+    this.width = 600;
+    this.height = 600;
     this.image = fireImage;
   }
 
   // Displaying the falling elf image
   display() {
     push();
-    // imageMode(CENTER);
+    imageMode(CENTER);
     image(this.image, this.x, this.y, this.width, this.height);
     pop();
   }
---
+++ projects/project2/progress/js/SnowflakeL5.js
@@ -0,0 +1,10 @@
+class SnowflakeL5 {
+  constructor(x, y) {
+    this.x = 0;
+    this.y = height/6;
+    this.vx = 0;
+    this.vy = 0;
+    this.size = 20;
+    this.speed = 5;
+  }
+}
